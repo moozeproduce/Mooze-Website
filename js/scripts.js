080ec53bb33bbcdd36e78ea64ec332dee9fd926c
Mooze <113169105+moozeproduce@users.noreply.github.com>
2023-02-21 23:09:49 -0600

    added all to repo

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const popupLink = document.getElementById("popup-link");
+const popup = document.getElementById("popup");
+const closeButton = document.getElementById("close-button");
+
+popupLink.addEventListener("click", function() {
+  popup.style.display = "block";
+});
+
+closeButton.addEventListener("click", function() {
+  popup.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+});
